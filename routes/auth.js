2debdf069def2f2573a51655d41e79ac3fd4c56a
Manjunath <makzink40@gmail.com>
2022-07-03 22:00:33 +0530

    changed access and refresh token

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const  router = require('express').Router();
+const router = require('express').Router();
 const User = require('../model/User')
 const md5 = require('md5')
 const { registerValidation, loginValidation, addProductValidation, updateProductValidation } = require('../validation')
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken')
 
 
 //regsitration route
-router.post('/registration',(req,res) => {
+router.post('/registration', (req, res) => {
     const { error } = registerValidation(req.body)
     if (error) {
         res.send(error.details[0].message);
@@ -30,7 +30,7 @@ router.post('/registration',(req,res) => {
 })
 
 //login route
-router.post('/login',(req,res)=>{
+router.post('/login', (req, res) => {
     const { error } = loginValidation(req.body)
 
     if (error) {
@@ -44,15 +44,18 @@ router.post('/login',(req,res)=>{
             else {
                 if (foundUser) {
                     if (foundUser.password === md5(req.body.password)) {
-                        const id = {id: foundUser.id}
-                        const accessToken = jwt.sign(id,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '15m'});
-                        const refreshToken = jwt.sign(id,process.env.REFRESH_TOKEN_SECRET,{expiresIn: '1d'});
-                        
+                        const id = { id: foundUser.id }
+                        ACCESS_TOKEN_SECRET = '172c6ea01f5b1e5e2e2d25813175ef788d26e873096a2fa6e4956a66119851a905f917b63ffe672889d67521240a4f272da2ef6a3ca87ea287e03596926787ec'
+                        REFRESH_TOKEN_SECRET = '96ea520fceff48464581a7753f4f828c49d1dc333afb92220015bdfbc15c0676bc5662ee00d65e812c6f69ecac2f1a029d93127105d9a618b10eb8d45c4b8e4f'
+
+                        const accessToken = jwt.sign(id, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+                        const refreshToken = jwt.sign(id, REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+
                         foundUser.tokens.push(refreshToken)
                         foundUser.save();
-                        res.cookie('jwt',refreshToken,{httpOnly:true});
+                        res.cookie('jwt', refreshToken, { httpOnly: true });
                         res.status(200).send(accessToken);
-                    
+
 
                     }
                     else {
