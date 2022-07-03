4d8ebdcc63884514f06438cf74aee765ecc3eadc
Manjunath <makzink40@gmail.com>
2022-07-03 21:51:08 +0530

    added methods

--- app.js
+++ app.js
@@ -8,25 +8,11 @@ const cookieParser = require('cookie-parser')
 
 
 
-// app.use(cors({credentials: true, origin: ['https://productapi.netlify.app','http://localhost:3000']}));
-app.use(function (req, res, next) {
+app.use(cors({credentials: true,
+    origin: 'https://productapi.netlify.app',
+    methods: ['GET', 'POST','PUT','PATCH','DELETE']
+}));
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://productapi.netlify.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 //importing routes
 const authRoute = require('./routes/auth')
